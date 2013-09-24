95ab9aaa31b6be45c771b9b5715027f060b82c1b
Moritz Zimmer <moritz.zmmr@gmail.com>
2013-09-24 09:40:41 +0200

    initial commit of the example project

---
+++ src/main/java/org/example/Foo.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.joda.time.DateTime;
+
+public class Foo {
+
+	private static final Log LOGGER = LogFactory.getLog(Foo.class);
+	
+	private DateTime bar;
+}
---
+++ src/test/java/org/example/FooTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class FooTest {
+
+	@Test
+	@Ignore("just an example")
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
